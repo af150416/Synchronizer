8c4e90bf3189724485af771970c482b1a76cdc49
af150416 <af150416@gmail.com>
2017-02-14 15:45:35 +0200

    up_code

--- src/main/java/com/softbistro/declarations/jparser/JParserApplication.java
+++ src/main/java/com/softbistro/declarations/jparser/JParserApplication.java
@@ -1,16 +1,38 @@
 package com.softbistro.declarations.jparser;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.softbistro.declarations.jparser.component.service.JparserDao;
 import com.softbistro.declarations.jparser.services.ParserService;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.softbistro.declarations.jparser")
+@EnableScheduling
+@ComponentScan(basePackages = "com.softbistro.declarations.jparser")
 public class JParserApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(JParserApplication.class, args);
-		new ParserService().getStagingDeclaration();		
-  }
+	private int getingPage = 0;
+
+	public static void main(String[] args)
+			throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+		ApplicationContext context = SpringApplication.run(JParserApplication.class, args);
+		ParserService parserService = context.getBean(ParserService.class);
+		parserService.getStagingDeclaration();
+//		ParsingJson parsingJson = new ParsingJson();
+//		parsingJson.getingInformationFromDeclaration();
+
+	}
+
+	// @Scheduled(fixedDelay = 1000)
+	public void show() {
+		JparserDao jparserDao = new JparserDao();
+		jparserDao.getListIdDeclaration(getingPage++);
+	}
 }
--- src/main/java/com/softbistro/declarations/jparser/services/ParserService.java
+++ src/main/java/com/softbistro/declarations/jparser/services/ParserService.java
@@ -2,10 +2,21 @@ package com.softbistro.declarations.jparser.services;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -16,48 +27,67 @@ import com.softbistro.declarations.jparser.entity.StagingDeclaration;
 @Service
 public class ParserService {
 
+	@Autowired
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	private final static String PARSED_URL = "https://public-api.nazk.gov.ua/v1/declaration/?page=%d";
+	private final static String SQL_REQUEST = "INSERT INTO stagingDeclaration (declaration_id, first_name, last_name, work_place, position, pdf_link, status)"
+			+ " VALUES (:declarationId, :firstName, :lastName, :placeOfWork, :position, :linkPdf, \"new\")";
+
 	public void getStagingDeclaration() {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-		for (int page = 439;; page++) {
+		boolean hasData = true;
+		for (int page = 1; hasData; page++) {
 			try {
-				String url = String.format("https://public-api.nazk.gov.ua/v1/declaration/?page=%d", page);
-				Response response = mapper.readValue(new URL(url), Response.class);
-				for(StagingDeclaration dec: response.getItems()){
-				saveStagingDeclaration(dec);
-				System.out.println("id:"+ dec.getId() + "wsa saved");
-				}
-
+				Response response = mapper.readValue(new URL(String.format(PARSED_URL, page)), Response.class);
+				saveListOfStagingDeclaration(response.getItems());
 				if (response.page.totalItems < (response.page.currentPage * response.page.batchSize)) {
-					break;
+					hasData = false;
 				}
-
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public void saveStagingDeclaration(StagingDeclaration dec) {
-		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-		try {
-			dataSource.setDriver(new com.mysql.jdbc.Driver());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		dataSource.setUrl("jdbc:mysql://sb-db01.softbistro.online/declaration?useSSL=false&useUnicode=yes&characterEncoding=UTF-8");
-		dataSource.setUsername("root");
-		dataSource.setPassword("rotrotrot");
+	public void saveListOfStagingDeclaration(List<StagingDeclaration> rawDeclarations) {
+		List<StagingDeclaration> declarations = new ArrayList<>();
+//		for (StagingDeclaration dec : rawDeclarations) {
+//			if (!isDuplicate(dec)) {
+//				declarations.add(dec);
+//				System.out.println(dec.getDeclarationId() + " Was add to save list");
+//			}
+//			else{System.out.println(dec.getDeclarationId() + " Already in Data Base");}
+//		}
 
-		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
-		String sqlInsert = "INSERT INTO stagingDeclaration (declaration_id, first_name, last_name, work_place, position, pdf_link, status)"
-				+ " VALUES (?, ?, ?, ?, ?, ?, ?)";
-		try {
-			jdbc.update(sqlInsert, dec.getId(), new String(dec.getFirstname().getBytes(), "UTF-8"), dec.getLastname(),
-					dec.getPlaceOfWork(), dec.getPosition(), dec.getLinkPDF(), "new");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(rawDeclarations.toArray());
+		System.out.println("writing to Data Base...");
+		namedParameterJdbcTemplate.batchUpdate(SQL_REQUEST, params);
+		System.out.println("Done");
+	}
+
+	// public boolean isDuplicate(StagingDeclaration dec) throws SQLException {
+	// Connection conn =
+	// DriverManager.getConnection("jdbc:mysql://sb-db01.softbistro.online/declaration?useuseUnicode=yes&characterEncoding=UTF-8"
+	// , "root", "rotrotrot");
+	// Statement stmt = conn.createStatement();
+	// ResultSet result = null;
+	// result = stmt.executeQuery("select * from stagingDeclaration where
+	// declaration_id="+("'"+dec.getDeclarationId()+"'"));
+	// if
+	// (result.isBeforeFirst()){System.out.println(dec.getDeclarationId()+"already
+	// in DataBase");}
+	// return result.isBeforeFirst();
+	// }
+
+	public boolean isDuplicate(StagingDeclaration dec) {
+		String sql = "select id from stagingDeclaration where declaration_id=?";
+		
+		return (Integer)jdbcTemplate.queryForObject(sql, new Object[] { dec.getDeclarationId() }, Integer.class) != null;
 	}
 }
