514036b2cd69d3da529ece151ce6a1ec748d1fce
af150416 <af150416@gmail.com>
2017-02-16 12:52:51 +0200

    functional parser

--- src/main/java/com/softbistro/declarations/jparser/entity/StagingDeclaration.java
+++ src/main/java/com/softbistro/declarations/jparser/entity/StagingDeclaration.java
@@ -1,81 +1,51 @@
 package com.softbistro.declarations.jparser.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Entity
-@Table(name="stagingDeclaration")
 public class StagingDeclaration {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Integer uid;
-	
-	@Column(name = "declaration_id")
-	private String id;
-	
-	@Column(name = "first_name")
-	private String firstname;
-	
-	@Column(name = "last_name")
-	private String lastname;
-	
-	@Column(name = "work_place")
+
+	@JsonProperty("id")
+	private String declarationId;
+
+	@JsonProperty("firstname")
+	private String firstName;
+
+	@JsonProperty("lastname")
+	private String lastName;
+
 	private String placeOfWork;
-	
-	@Column(name = "position")
+
 	private String position;
-	
-	@Column(name = "pdf_link")
-	private String linkPDF;
-	
-	@Column(name = "status")
-	private String status;
-	
-	public StagingDeclaration(){
-		
+
+	@JsonProperty("linkPDF")
+	private String linkPdf;
+
+	private enum Status {
+		NEW, UPDATE, IN_PROGRESS, DONE
+	};
+
+	public String getDeclarationId() {
+		return declarationId;
 	}
 
-	public StagingDeclaration(String id, String firstname, String lastname, String placeOfWork, String position,
-			String linkPDF) {
-		super();
-		this.id = id;
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.placeOfWork = placeOfWork;
-		this.position = position;
-		this.linkPDF = linkPDF;
-		this.status = "new";
-	}
-	
-	
-	public String getId() {
-		return id;
+	public void setDeclarationId(String declarationId) {
+		this.declarationId = declarationId;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public String getFirstname() {
-		return firstname;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+	public String getLastName() {
+		return lastName;
 	}
 
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public String getPlaceOfWork() {
@@ -94,25 +64,17 @@ public class StagingDeclaration {
 		this.position = position;
 	}
 
-	public String getLinkPDF() {
-		return linkPDF;
+	public String getLinkPdf() {
+		return linkPdf;
 	}
 
-	public void setLinkPDF(String linkPDF) {
-		this.linkPDF = linkPDF;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
+	public void setLinkPdf(String linkPdf) {
+		this.linkPdf = linkPdf;
 	}
 
 	@Override
-	public String toString(){
-		return new StringBuilder()+getLastname()+" "+getFirstname()+" "+getId();
-		
+	public String toString() {
+		return new StringBuilder() + getLastName() + " " + getFirstName() + " " + getDeclarationId();
+
 	}
 }
--- src/main/java/com/softbistro/declarations/jparser/services/ParserService.java
+++ src/main/java/com/softbistro/declarations/jparser/services/ParserService.java
@@ -2,62 +2,56 @@ package com.softbistro.declarations.jparser.services;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softbistro.declarations.jparser.entity.Response;
-import com.softbistro.declarations.jparser.entity.StagingDeclaration;
 
+/**
+ * Parsing JSON with declaration
+ * 
+ * @author af150416
+ *
+ */
 @Service
 public class ParserService {
 
-	public void getStagingDeclaration() {
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private StagingDeclarationDAOService stagingDeclarationDAOService;
+
+	private Logger logger = Logger.getLogger(ParserService.class.getName());
+
+	/**
+	 * Scheduled method for regular parsing declaration by cron job task
+	 */
+	@Scheduled(cron = "0 0 1 * * *")
+	public void parseDeclaration() {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-		for (int page = 439;; page++) {
+		Response response = null;
+		int page = 1;
+		do {
 			try {
-				String url = String.format("https://public-api.nazk.gov.ua/v1/declaration/?page=%d", page);
-				Response response = mapper.readValue(new URL(url), Response.class);
-				for(StagingDeclaration dec: response.getItems()){
-				saveStagingDeclaration(dec);
-				System.out.println("id:"+ dec.getId() + "wsa saved");
-				}
-
-				if (response.page.totalItems < (response.page.currentPage * response.page.batchSize)) {
-					break;
-				}
-
+				response = mapper.readValue(new URL(environment.getProperty("parsed.url") + page), Response.class);
+				logger.log(Level.INFO,
+						String.format("Page %d writing to Data Base...", response.page.getCurrentPage()));
+				stagingDeclarationDAOService.save(response.getItems());
+				page++;
 			} catch (IOException e) {
-				e.printStackTrace();
+				logger.log(Level.SEVERE, e.toString(), e);
 			}
-		}
-	}
-
-	public void saveStagingDeclaration(StagingDeclaration dec) {
-		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-		try {
-			dataSource.setDriver(new com.mysql.jdbc.Driver());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		dataSource.setUrl("jdbc:mysql://sb-db01.softbistro.online/declaration?useSSL=false&useUnicode=yes&characterEncoding=UTF-8");
-		dataSource.setUsername("root");
-		dataSource.setPassword("rotrotrot");
-
-		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
-		String sqlInsert = "INSERT INTO stagingDeclaration (declaration_id, first_name, last_name, work_place, position, pdf_link, status)"
-				+ " VALUES (?, ?, ?, ?, ?, ?, ?)";
-		try {
-			jdbc.update(sqlInsert, dec.getId(), new String(dec.getFirstname().getBytes(), "UTF-8"), dec.getLastname(),
-					dec.getPlaceOfWork(), dec.getPosition(), dec.getLinkPDF(), "new");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		} while (response.page.totalItems > (response.page.currentPage * response.page.batchSize));
+		logger.log(Level.INFO, String.format(" <<< All pages was writing to Data Base >>> "));
 	}
 }
